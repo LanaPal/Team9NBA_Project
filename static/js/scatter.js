be6f504ac3b292db16b270a1655a5227e2083ae6
LanaPal <64497295+LanaPal@users.noreply.github.com>
2020-09-14 20:49:52 -0500

    scatter plot changes

--- static/js/scatter.js
+++ static/js/scatter.js
@@ -1,23 +1,19 @@
-// console.log('here')
-// const url = "mongodb://localhost:27017/nba_db";
-// const url = '/tweets'
-
-  // chart building
-const svgWidth = 960;
+// Step 1. chart building
+const svgWidth = 500
 const svgHeight = 500;
 
 const margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
+  top: 10,
+  right: 10,
+  bottom: 10,
+  left: 10
   };
 
-const width = 960 - margin.left - margin.right;
+const width = 500 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-const svg = d3.select("#scatter")
+const svg = d3.select("#chart")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -69,8 +65,8 @@ d3.json(get_player_stats).then(function(player_stats_data) {
       .append("circle")
       .attr("cx", d => xLinearScale(d.wins))
       .attr("cy", d => yLinearScale(d.SALARY_MILLIONS))
-      .attr("r", "15")
-      .attr("fill", "blue")
+      .attr("r", "10")
+      .attr("fill", "CornflowerBlue")
       .attr("opacity", ".5");
      
     // Step 6: Initialize tool tip
@@ -98,7 +94,7 @@ d3.json(get_player_stats).then(function(player_stats_data) {
      // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
+      .attr("y", 0 - margin.left + 0)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
@@ -108,6 +104,15 @@ d3.json(get_player_stats).then(function(player_stats_data) {
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
       .text("Wins per Season");
+
+    svg.append("text")
+      .attr("x", (width /2))
+      .attr("y", 0 - (margin.top /2))
+      .attr("text-anchor", "middle")
+      .style("font-size", "14px")
+      .style("text-decoration", "underline")
+      .text("Salary in Millions vs Game Wins")
+
     }).catch(function (error) {
     console.log(error);
   });
